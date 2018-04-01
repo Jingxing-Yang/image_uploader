77395033252ebe9b2ff53a9ec322a37870c06491
Jingxing Yang <jingxiny@usc.edu>
2018-04-01 06:49:43 -0700

    image-uploader

---
+++ upload.js
@@ -0,0 +1,25 @@
+
+var fs = require('fs');
+var path = "/home/pi/transmit/1.jpg";
+
+
+
+//var postData = {pic:inputFile};
+var request = require('request');
+
+function upload()
+{
+    var imageFile = fs.readFileSync(path);
+    // Covert the image data to a Buffer and base64 encode it.
+    var inputFile = new Buffer(imageFile).toString('base64');
+    request.post(
+        'http://35.185.235.43:3000/pic',
+         { json: { pic: inputFile } },
+        function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                console.log(body)
+            }
+        }
+    );
+}
+setInterval(upload,500);
